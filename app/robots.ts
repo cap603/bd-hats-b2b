0f1d24b78f1cd6e0b3fe6383b3a1871ecadc1368
cap603 <3779980382@qq.com>
2026-06-10 11:27:21 +0800

    seo: add robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,11 @@
 import { MetadataRoute } from 'next'
+
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: '*', allow: '/' }, sitemap: 'https://b2b.bdjunyang.com/sitemap.xml' }
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://b2b.bdjunyang.com/sitemap.xml',
+  }
 }
