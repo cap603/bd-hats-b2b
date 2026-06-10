ab41ac0dbb4cfd2e528579e42593f4ff1b6deb4e
cap603 <3779980382@qq.com>
2026-06-10 14:39:26 +0800

    chore: bust build cache to compile tailwind CSS

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    CACHE_BUST: "2026-06-10",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+};
 
+export default nextConfig;
