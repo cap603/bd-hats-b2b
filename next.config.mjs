afcc4612a497184c0645494d420cfc05fb70a444
cap603 <3779980382@qq.com>
2026-06-10 15:05:49 +0800

    chore: bust build cache to compile tailwind CSS v3

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    CACHE_BUST: "2026-06-10-v2",
+    CACHE_BUST: "2026-06-10-v3",
   },
   images: {
     remotePatterns: [
