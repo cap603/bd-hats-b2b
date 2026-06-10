9d497a07a680871d1ff8b0cb92f80fdddce68741
cap603 <3779980382@qq.com>
2026-06-10 09:41:52 +0800

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,4 @@
+import { MetadataRoute } from 'next'
+export default function robots(): MetadataRoute.Robots {
+  return { rules: { userAgent: '*', allow: '/' }, sitemap: 'https://b2b.bdjunyang.com/sitemap.xml' }
+}
