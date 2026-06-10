4bc4da5b709648a5600158516a4ed72a2fccdf2e
cap603 <3779980382@qq.com>
2026-06-10 11:27:08 +0800

    seo: add sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,12 @@
 import { MetadataRoute } from 'next'
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [{ url: 'https://b2b.bdjunyang.com', lastModified: new Date(), changeFrequency: 'weekly', priority: 1 }]
+  return [
+    {
+      url: 'https://b2b.bdjunyang.com',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
 }
