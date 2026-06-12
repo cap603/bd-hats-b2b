9ac42a2defdfb9613f195bccd5b38aa501cb3b3c
cap603 <3779980382@qq.com>
2026-06-12 15:04:55 +0800

    chore: bust cache v11

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    CACHE_BUST: "2026-06-10-v10",
+    CACHE_BUST: "2026-06-10-v11",
   },
   images: {
     remotePatterns: [
@@ -21,6 +21,4 @@ const nextConfig = {
   },
 };
 
-
 export default nextConfig;
-
